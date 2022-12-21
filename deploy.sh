6005e85af036edd02efacafcd8eb9d7651e8d611
Mohammed Chammam <mohammed.chammam@gmail.com>
2022-12-21 12:55:43 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
